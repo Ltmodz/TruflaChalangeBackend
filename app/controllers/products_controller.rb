0d4c785813064a442439593cc764a55a5682b170
modz <Ltmodz@github.com>
2020-02-10 00:45:26 +0200

    added the total number of data to be returned with te request

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,26 +1,14 @@
 # frozen_string_literal: true
 
 class ProductsController < ApplicationController
-  before_action :filterProducts
-  def index 
-    byebug
-    products = @products || Product.includes(:promotions).page(1)
-    render json: products
-  end
-
-  def total_count
-    render json: Product.count
-  end
-
-  private
-
-  def productParams
-    params.permit(:q)
-  end
-
-  def filterProducts
-      @products = Product.where("name LIKE '%#{params[:q]}%'")  if productParams[:q] &&  !productParams[:q].blank?
-      @products = Department.find(productParams[:department_id]).products  if productParams[:department_id] 
-      @products = Product.includes(:promotions).where(promotions: { code: productParams[:promo_code] })   if productParams[:promo_code] &&  !productParams[:promo_code].blank?
+  def index
+    render json: {
+      products: ActiveModel::Serializer::CollectionSerializer.new(
+        Product.page(params[:page])
+        .includes(:promotions).per(15).ransack(params[:q]).result,
+        each_serializer: ProductSerializer
+      ),
+      total_count: Product.ransack(params[:q]).result.count
+    }
   end
 end
--- app/serializers/product_serializer.rb
+++ app/serializers/product_serializer.rb
@@ -1,4 +1,6 @@
+# frozen_string_literal: true
+
 class ProductSerializer < ActiveModel::Serializer
   has_many :promotions
-  attributes :id,:name, :price 
+  attributes :id, :name, :price
 end
